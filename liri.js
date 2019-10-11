a51055f2c27c1732bf76fb1177883f5bf9a4d936
Lucio Verane <verane64@gmail.com>
2019-10-11 17:50:03 -0500

    spotify search added, basic instructions on empty command added, log now includes search result, all searches now have default values

--- liri.js
+++ liri.js
@@ -1,34 +1,43 @@
 require("dotenv").config();
 var fs = require('fs');
 var keys = require("./keys.js");
-
+var axios = require("axios");
+var moment = require('moment');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-
-var axios = require("axios");
-
-var moment = require('moment');
 
 var userRequest = process.argv[2];
 var userSearch = process.argv.slice(3).join("+");
 
-var artistName = [];
-var userArtistName;
 var resultArtistName;
-function fixName() {
-    userArtistName = userSearch.split("+")
-    for (var i = 0; i < userArtistName.length; i++) {
-        artistName[i] = userArtistName[i].charAt(0).toUpperCase() + userArtistName[i].slice(1);
+function fixName(lowerName) {
+    var artistName = [];
+    lowerName = lowerName.split("+")
+    for (var i = 0; i < lowerName.length; i++) {
+        artistName[i] = lowerName[i].charAt(0).toUpperCase() + lowerName[i].slice(1);
     }
     resultArtistName = artistName.join(" ");
-    console.log(resultArtistName)
 }
 
-//
 
-console.log("------------------------------------------")
+function action(textToPrint) {
+    console.log(textToPrint)
+    fs.appendFile("log.txt", textToPrint + ",\n", function () { })
+}
 
-baseSwitch();
+
+if (process.argv.length === 2) {
+    console.log("enter 'concert-this' and a band/artist name to get information on their upcoming shows")
+    console.log("enter 'spotify-this-song' and a song name to get information on it")
+    console.log("enter 'movie-this' and a movie title to get information on it")
+    console.log("enter 'do-what-it-says' for a surprise")
+    console.log("no quotes are needed, and you may enter only the first command for a default result")
+
+} else {
+    fs.appendFile("log.txt", "user input was " + process.argv.slice(2).join(" ") + ",\n", function () { })
+    action("------------------------------------------");
+    baseSwitch();
+}
 
 function baseSwitch() {
     switch (userRequest) {
@@ -55,38 +64,45 @@ function baseSwitch() {
 }
 
 function concert() {
-    fixName();
+    if (userSearch === "") {
+        userSearch = "maroon+5";
+    }
+    fixName(userSearch);
     var queryUrl = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp"
-    console.log(queryUrl)
     axios.get(queryUrl).then(
         function (response) {
-            // console.log(response.data[1])
-            console.log("Upcoming " + resultArtistName + " shows")
-            for (var i = 0; i < 10; i++) {
-                // console.log(response.data[i])
-                console.log("Venue: " + response.data[i].venue.name)
+            action("Upcoming " + resultArtistName + " shows")
+            for (var i = 0; i < 10 && i < response.data.length; i++) {
+                action("Venue: " + response.data[i].venue.name)
                 if (response.data[i].venue.region === "") {
-                    console.log("Venue location: " + response.data[i].venue.country + ", " + response.data[i].venue.city)
+                    action("Venue location: " + response.data[i].venue.country + ", " + response.data[i].venue.city)
                 } else {
-                    console.log("Venue location: " + response.data[i].venue.country + ", " + response.data[i].venue.region + ", " + response.data[i].venue.city)
+                    action("Venue location: " + response.data[i].venue.country + ", " + response.data[i].venue.region + ", " + response.data[i].venue.city)
                 }
                 var date = response.data[i].datetime
-                console.log("Date: " + moment(date).format("MM/DD/YYYY"))
-                console.log("------------------------------------------")
+                action("Date: " + moment(date).format("MM/DD/YYYY"))
+                action("------------------------------------------")
             }
         }
     )
 }
 
 function spotifySong() {
-    console.log("spotify is not yet implemented")
-    console.log(userSearch)
-    // var queryUrl = "https://api.spotify.com/v1/search?q=" + userSearch + "&type=artist"
-    // axios.get(queryUrl).then(
-    //     function (response) {
-    //         console.log(response)
-    //     }
-    // )
+    if (userSearch === "") {
+        userSearch = "the+sign+us+album";
+    }
+    spotify.search({ type: 'track', query: userSearch, limit: 1 }, function (err, data) {
+        if (err) {
+            return action('Error occurred: ' + err);
+        }
+        // console.log(data.tracks.items)        
+        var inputName = data.tracks.items[0].artists[0].name
+        fixName(inputName);
+        action("Artist name: " + resultArtistName);
+        action("Song name: " + data.tracks.items[0].name);
+        action("Preview link: " + data.tracks.items[0].external_urls.spotify);
+        action("Album: " + data.tracks.items[0].album.name);
+    });
 }
 
 function movie() {
@@ -94,18 +110,16 @@ function movie() {
         userSearch = "mr+nobody";
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + userSearch + "&apikey=trilogy";
-    console.log(queryUrl)
     axios.get(queryUrl).then(
         function (response) {
-            // console.log(response.data);
-            console.log("Title: " + response.data.Title);
-            console.log("Year of release: " + response.data.Year);
-            console.log("IMDB rating: " + response.data.imdbRating);
-            console.log(response.data.Ratings[1].Source + " rating: " + response.data.Ratings[1].Value);
-            console.log("Country: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
+            action("Title: " + response.data.Title);
+            action("Year of release: " + response.data.Year);
+            action("IMDB rating: " + response.data.imdbRating);
+            action(response.data.Ratings[1].Source + " rating: " + response.data.Ratings[1].Value);
+            action("Country: " + response.data.Country);
+            action("Language: " + response.data.Language);
+            action("Plot: " + response.data.Plot);
+            action("Actors: " + response.data.Actors);
         }
     )
 }
@@ -113,7 +127,7 @@ function movie() {
 function random() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
-            return console.log(error);
+            return action(error);
         }
         var randomPosition = Math.floor(Math.random() * 3)//0, 1, 2
         var position = randomPosition * 2;//0, 2, 4
@@ -127,13 +141,3 @@ function random() {
         baseSwitch();
     })
 }
-
-
-fs.appendFile("log.txt", "userRequest was " + userRequest + ",\n" + "userSearch was " + userSearch + ",\n", function(err) {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    console.log("Content Added!");
-  }
-});
